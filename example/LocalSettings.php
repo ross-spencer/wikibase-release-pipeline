df4fc8dfbb90dc0886b732de89bfe114b84b3ee0
ross-spencer <all.along.the.watchtower2001@gmail.com>
2021-09-08 09:10:20 +0000

    WIP: Configuration for Wikibase/Siegfried

--- example/LocalSettings.php
+++ example/LocalSettings.php
@@ -1,9 +1,57 @@
 <?php
 
+$wgSitename = "Wikibase Siegfried Demo";
+$wgLanguageCode = "en";
+
 /*******************************/
 /* Enable Federated properties */
 /*******************************/
-#$wgWBRepoSettings['federatedPropertiesEnabled'] = true;
+//$wgWBRepoSettings['federatedPropertiesEnabled'] = true;
+
+//wfLoadExtension( 'WikibaseImport' );
+//wfLoadExtension( 'WikibaseLocalMedia' );
+
+$wgGroupPermissions['*']['edit'] = false;
+
+/*******************************/
+/* Editing and Permissions     */
+/*******************************/
+
+// Disable anonymous editing
+$wgGroupPermissions['*']['edit'] = false;
+
+/*******************************/
+/* Extension based features    */
+/*******************************/
+
+$wgEnableUploads = true;
+
+$wgLogos = [
+	'1x' => "$wgScriptPath/assets/logo/wiki.svg",
+	'1.5x' => "$wgScriptPath/assets/logo/wiki.svg",
+	'2x' => "$wgScriptPath/assets/logo/wiki.svg",
+	'svg' =>  "$wgScriptPath/assets/logo/wiki.svg",
+];
+
+// Relative to www folder and without trailing slashes...
+$wgUploadDirectory = "$IP/uploads";
+$wgUploadPath = "$wgScriptPath/uploads";
+
+// File uploads:
+$wgCheckFileExtensions = false;
+$wgMaxUploadSize = 106000000;
+
+// Namespaces, e.g. for Digital Preservation or Collection Care?
+// $wgExtraNamespaces = [ 'digipres' ];
+
+
+/* RC Age
+ https://www.mediawiki.org/wiki/Manual:
+ Items in the recentchanges table are periodically purged; entries older than this many seconds will go.
+ The query service (by default) loads data from recent changes
+ Set this to 1 year to avoid any changes being removed from the RC table over a shorter period of time.
+*/
+$wgRCMaxAge = 365 * 24 * 3600;
 
 /*******************************/
 /* Enables ConfirmEdit Captcha */
@@ -23,4 +71,5 @@
 /*******************************/
 /* Disable UI error-reporting  */
 /*******************************/
-#ini_set( 'display_errors', 0 );
+
+# ini_set( 'display_errors', 0 );
