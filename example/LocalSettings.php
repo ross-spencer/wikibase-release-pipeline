f383e52e62fbb8d90569e018dc4306e5ebef0e87
ross-spencer <all.along.the.watchtower2001@gmail.com>
2021-09-25 20:20:26 +0000

    WIP: Other config

--- example/LocalSettings.php
+++ example/LocalSettings.php
@@ -3,7 +3,43 @@
 /*******************************/
 /* Enable Federated properties */
 /*******************************/
-#$wgWBRepoSettings['federatedPropertiesEnabled'] = true;
+$wgWBRepoSettings['federatedPropertiesEnabled'] = true;
+
+wfLoadExtension( 'WikibaseImport' );
+wfLoadExtension( 'WikibaseLocalMedia' );
+
+/*******************************/
+/* Extension based features    */
+/*******************************/
+
+$wgEnableUploads = true;
+
+$wgLogos = [
+	'1x' => "$wgScriptPath/assets/logo/wiki.svg",
+	'1.5x' => "$wgScriptPath/assets/logo/wiki.svg",
+	'2x' => "$wgScriptPath/assets/logo/wiki.svg",
+	'svg' =>  "$wgScriptPath/assets/logo/wiki.svg",
+];
+
+// Relative to www folder and without trailing slashes...
+$wgUploadDirectory = "$IP/uploads";
+$wgUploadPath = "$wgScriptPath/uploads";
+
+// File uploads:
+$wgCheckFileExtensions = false;
+$wgMaxUploadSize = 106000000;
+
+// Namespaces, e.g. for Digital Preservation or Collection Care?
+// $wgExtraNamespaces = [ 'digipres' ];
+
+
+/* RC Age
+ https://www.mediawiki.org/wiki/Manual:
+ Items in the recentchanges table are periodically purged; entries older than this many seconds will go.
+ The query service (by default) loads data from recent changes
+ Set this to 1 year to avoid any changes being removed from the RC table over a shorter period of time.
+*/
+$wgRCMaxAge = 365 * 24 * 3600;
 
 /*******************************/
 /* Enables ConfirmEdit Captcha */
